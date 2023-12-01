09d4191a70feabd2170480cf9ff9ccdbed903045
Caladan8 <tnambiar2008@gmail.com>
2023-12-01 15:36:25 +0530

    Add readex font

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,5 @@
 <div class="flex justify-between">
   <div class="p-2">
-    <img src="/RCIS_Logo.png" alt="rcis logo" class="h-28 w-[8.75rerm]">
+    <img src="/RCIS_Logo.png" alt="rcis logo" class="h-28 w-[8.75rem]">
   </div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,3 +5,7 @@
 
 <Navbar />
 <slot />
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200;300;400;500;600;700&display=swap');
+</style>
