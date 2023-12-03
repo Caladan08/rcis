0fb12e3e97abc12b740258fc312b6c374500b14f
Caladan8 <tnambiar2008@gmail.com>
2023-12-03 15:17:02 +0530

    Wrap <slot /> with SvelteUIProvider

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script>
 	import '../app.pcss';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import { SvelteUIProvider } from '@svelteuidev/core';
 </script>
 
-<Navbar />
-<slot />
+<SvelteUIProvider>
+	<Navbar />
+	<slot />
+</SvelteUIProvider>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200;300;400;500;600;700&display=swap');
