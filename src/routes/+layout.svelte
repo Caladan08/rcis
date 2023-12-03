d63401389e73f1148fbeda9de4780867ddad2553
Caladan8 <tnambiar2008@gmail.com>
2023-12-03 15:33:12 +0530

    Remove svelteui components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,10 @@
 <script>
 	import '../app.pcss';
 	import Navbar from '$lib/components/Navbar.svelte';
-	import { SvelteUIProvider } from '@svelteuidev/core';
 </script>
 
-<SvelteUIProvider>
-	<Navbar />
-	<slot />
-</SvelteUIProvider>
+<Navbar />
+<slot />
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200;300;400;500;600;700&display=swap');
