75f099230fd98b26b23dd4c8f880b3234cd70fcb
Caladan8 <tnambiar2008@gmail.com>
2023-12-04 22:48:09 +0530

    Add side drawer for menu

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,26 @@
+<script lang="ts">
+	import { Drawer } from "flowbite-svelte"
+	import { sineIn } from 'svelte/easing';
+
+  let hidden = true;
+  let transitionParams = {
+    x: 320,
+    duration: 200,
+    easing: sineIn
+  };
+</script>
+
 <div class="flex justify-between">
 	<div class="p-2">
 		<img src="/RCIS_Logo.png" alt="rcis logo" class="h-28 w-[8.75rem]" />
 	</div>
+	<div class="p-2">
+		<button on:click={() => hidden = false}>Click</button>
+	</div>
 </div>
+
+<Drawer placement="right" transitionType="fly" {transitionParams} bind:hidden={hidden} id="sidebar1">
+  <div class="content">
+		<h1>Content</h1>
+	</div>
+</Drawer>
