6ce54368716a9be52c4f68bbd92dc0bda5d7afe0
Caladan8 <tnambiar2008@gmail.com>
2023-12-05 22:33:35 +0530

    Change spacing of items on navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
-	import { Drawer } from "flowbite-svelte"
+	import { Drawer } from 'flowbite-svelte';
 	import { sineIn } from 'svelte/easing';
+	import Menu from 'carbon-icons-svelte/lib/Menu.svelte';
 
-  let hidden = true;
-  let transitionParams = {
-    x: 320,
-    duration: 200,
-    easing: sineIn
-  };
+	let hidden = true;
+	let transitionParams = {
+		x: 320,
+		duration: 200,
+		easing: sineIn
+	};
 </script>
 
-<div class="flex justify-between">
-	<div class="p-2">
-		<img src="/RCIS_Logo.png" alt="rcis logo" class="h-28 w-[8.75rem]" />
+<div class="flex justify-between px-12 py-6">
+	<div>
+		<img src="/RCIS_Logo.png" alt="rcis logo" class="w-28" />
 	</div>
-	<div class="p-2">
-		<button on:click={() => hidden = false}>Click</button>
+	<div class="flex items-center">
+		<button on:click={() => (hidden = false)} ><Menu size={44} /></button>
 	</div>
 </div>
 
-<Drawer placement="right" transitionType="fly" {transitionParams} bind:hidden={hidden} id="sidebar1">
-  <div class="content">
+<Drawer placement="right" transitionType="fly" {transitionParams} bind:hidden id="sidebar1">
+	<div class="content">
 		<h1>Content</h1>
 	</div>
 </Drawer>
