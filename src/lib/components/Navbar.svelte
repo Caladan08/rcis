5e67291cebf295ab182574a959ec8d33c8385bc5
Caladan8 <tnambiar2008@gmail.com>
2023-12-06 22:21:21 +0530

    Remove side navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,25 @@
+<script lang="ts">
+  import { Drawer } from 'flowbite-svelte';
+	import { sineIn } from 'svelte/easing';
+	import Menu from 'carbon-icons-svelte/lib/Menu.svelte';
+
+	let hidden = true;
+	let transitionParams = {
+		x: 320,
+		duration: 200,
+		easing: sineIn
+	};
+</script>
+
 <div class="flex justify-between px-12 py-6">
 	<div>
 		<img src="/RCIS_Logo.png" alt="rcis logo" class="w-28" />
 	</div>
+	<button on:click={() => (hidden = false)} class="flex py-6 "><Menu size={32} /></button>
 </div>
+
+<Drawer placement="right" transitionType="fly" {transitionParams} bind:hidden id="sidebar1">
+	<div class="content">
+		<h1>Content</h1>
+	</div>
+</Drawer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,10 @@
 <script>
 	import '../app.pcss';
 	import Navbar from '$lib/components/Navbar.svelte';
-	import SideNavbar from '$lib/components/SideNavbar.svelte';
 </script>
 
-<div class="flex">
-	<div>
-		<Navbar />
-		<slot />
-	</div>
-	<SideNavbar />
-</div>
+<Navbar />
+<slot />
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200;300;400;500;600;700&display=swap');
