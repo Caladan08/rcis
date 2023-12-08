7f697860a5b0cc67b650fa1e374781848bb563bb
Caladan8 <tnambiar2008@gmail.com>
2023-12-08 23:15:15 +0530

    Remove lorem text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<h1 class="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda rerum architecto ab laborum animi sed debitis nisi dignissimos. Inventore porro unde ea, id doloremque velit et eum officia quod dolore!
-Magnam, perferendis consectetur expedita atque optio maiores voluptatum eum, ratione ipsum voluptate quas suscipit inventore esse aspernatur quo dolores dicta cum. Obcaecati dicta molestiae, nam natus impedit ex deserunt dolore?
-Deserunt dolores eaque voluptas aliquam ducimus officia dicta accusantium aliquid facere quod a sapiente at doloremque odit cumque consequuntur soluta tempora minus, doloribus temporibus illum iste? Quam architecto enim inventore!
-Natus illo aut accusamus reprehenderit officiis fugit nobis laboriosam id repudiandae mollitia laudantium fugiat quasi doloribus suscipit, voluptatibus corporis nam obcaecati? Omnis cumque, quas debitis distinctio aperiam nulla explicabo ipsum?
-Ducimus quidem voluptatibus dolore error veniam quam, aut ad sint laborum qui, beatae quos minima culpa ut consequatur commodi, aliquid sapiente laboriosam assumenda. Dolorum, accusamus. Aut, in! Quas, dolore nostrum.
-Asperiores quo consequatur porro libero blanditiis nam voluptatum accusamus laudantium soluta ex cupiditate quod officiis fugit commodi numquam necessitatibus facilis enim molestias, aliquid culpa dignissimos eos harum voluptates dolore! Distinctio?
-Sequi nostrum porro quidem numquam culpa aut! Porro aspernatur sint quibusdam! Facilis consectetur sint, minus est sequi unde, reprehenderit reiciendis officiis maxime explicabo quidem suscipit? Aut, dolor rerum? Voluptatem, sed?
-Nobis rem totam porro, unde nisi tenetur temporibus nemo quia illum voluptate et perspiciatis autem voluptates id laudantium accusantium! Earum consequatur laboriosam repudiandae labore ipsa vitae nobis suscipit consequuntur temporibus!
-Esse rerum reprehenderit at recusandae, quo eos totam inventore id illo mollitia possimus sint fugiat suscipit dolorem modi ipsum architecto autem ducimus quis debitis! Non at impedit maxime similique sit?
-Eos ullam fugit quasi deleniti dolorum voluptate est perspiciatis. Ab accusantium reprehenderit itaque dolor, assumenda rerum voluptatum quidem voluptas quos eius. Optio vitae voluptate sequi aliquid laboriosam voluptatibus accusamus quibusdam.
-Iusto ad id repellat enim maiores excepturi voluptatibus eveniet quis nulla, vero magni eos, soluta odio animi molestiae nisi laboriosam natus nemo ipsum velit? Harum ad suscipit quo quisquam impedit.
-Dicta cumque enim odit voluptate est, sit alias ipsum velit recusandae soluta ex aut nam autem saepe aliquid. Iusto atque quod esse tenetur totam aliquid. Id iusto quibusdam doloribus eaque?
-Quasi, fugiat. Saepe nisi officiis aperiam rerum laboriosam, reprehenderit quidem voluptates maxime earum, ut possimus voluptatum iure omnis itaque recusandae similique ipsam pariatur! Saepe qui exercitationem esse, aliquid provident dolores.
-Non nulla, fuga ratione repudiandae voluptas quis laborum quibusdam sit debitis repellendus minima expedita dolores doloremque dolore enim pariatur inventore. Tenetur odit id quae quos laboriosam nam ullam expedita dicta.
-Odio autem reiciendis itaque corrupti eos. Quod quos, eveniet iusto in vel, ullam natus numquam dolorum nesciunt eum inventore dicta. Maxime, sequi? Consequuntur similique deleniti eaque id ipsam, mollitia inventore!
-Fugiat sint harum accusantium aliquid autem deleniti velit quam sapiente! Vero nemo dolor rem reprehenderit sit praesentium aliquid! Sunt voluptate, quidem quibusdam laborum tempore rerum dolore soluta ratione consectetur itaque.
-Hic perferendis unde iusto exercitationem doloribus voluptatum. Error eius esse vel nisi modi, ea unde voluptates eaque, aperiam obcaecati explicabo excepturi iusto quisquam veritatis beatae, velit sunt? Ipsa, cupiditate beatae!
-Ipsam nesciunt consectetur dicta. Esse alias porro fuga quam repellendus id cupiditate provident, deserunt minus autem possimus. Rem, odio eos cupiditate dolor, exercitationem dicta quos doloribus at quidem et veritatis.
-Corporis, explicabo placeat. Culpa laborum repellat ut unde magni ex facilis iusto nostrum natus placeat sed voluptate inventore blanditiis, obcaecati accusantium? Unde sequi sint soluta odio consectetur aliquam est totam.
-Doloribus nesciunt, nam distinctio, magnam similique possimus modi repellat, dicta veritatis ad enim impedit consectetur! Esse ratione sapiente excepturi, velit ducimus distinctio, architecto et earum qui nihil quod recusandae impedit.</h1>
-
-
 <style>
   * {
     font-family: 'Readex Pro', sans-serif;
