99d2b209b31f882546a70620846e17797232dd58
Caladan8 <tnambiar2008@gmail.com>
2024-02-23 22:28:21 +0530

    Add contact info to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,12 @@
-<div class="bg-red w-full p-12">
-	<div class="w-full flex">
-		<div class="flex gap-56">
+<script>
+	import Location from 'carbon-icons-svelte/lib/Location.svelte';
+	import PhoneFilled from 'carbon-icons-svelte/lib/PhoneFilled.svelte';
+	import EmailNew from 'carbon-icons-svelte/lib/EmailNew.svelte';
+</script>
+
+<div class="bg-red w-full p-4">
+	<div class="w-full flex border-b-[1px] border-solid border-white/50">
+		<div class="flex gap-48 border-r-[1px] border-solid border-white/50 p-8 w-4/6">
 			<div class="flex flex-col gap-2">
 				<h1 class="uppercase text-white text-2xl font-extrabold">about us</h1>
 				<hr />
@@ -37,7 +43,44 @@
 				>
 			</div>
 		</div>
-		<div class="flex"></div>
+		<div class="flex p-8 flex-col text-white w-2/6">
+			<div class="flex gap-2 items-center mb-4">
+				<Location size={32} />
+				<p class="font-extrabold inline text-2xl">RCIS, Kalyan Nagar</p>
+			</div>
+			<div class="ml-6 mb-2">
+				<div class="flex gap-2 items-center">
+					<PhoneFilled size={46} />
+					<h1>
+						080-25435458/ 080-25435459/ 080-25435454/ 080-25535455/ +91 9844702444 / +91 9535216115
+					</h1>
+				</div>
+			</div>
+			<div class="ml-6">
+				<div class="flex gap-2 items-center">
+					<EmailNew size={24} />
+					<h1>info@rcis.in</h1>
+				</div>
+			</div>
+			<br />
+			<br>
+			<div class="flex gap-2 items-center mb-4">
+				<Location size={32} />
+				<p class="font-extrabold inline text-2xl">RCIS, Chamrajpet (CBSE)</p>
+			</div>
+			<div class="ml-6 mb-2">
+				<div class="flex gap-2 items-center">
+					<PhoneFilled size={39} />
+					<h1>+91 9148820597/ 080-29745680/ 080-29744088/ 080-29744383/ +91 9071 399 777</h1>
+				</div>
+			</div>
+			<div class="ml-6">
+				<div class="flex gap-2 items-center">
+					<EmailNew size={24} />
+					<h1>inforcis07@gmail.com</h1>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 
