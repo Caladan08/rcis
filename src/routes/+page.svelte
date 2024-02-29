2efeb78a37e4caec884e86c6c0b8e7bc029de2d9
Caladan8 <tnambiar2008@gmail.com>
2024-02-29 10:47:51 +0530

    Replace hero section carousel with static image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,39 +3,6 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import { fly } from 'svelte/transition';
 
-	const images = [
-		{
-			alt: 'test',
-			src: 'https://rcis.in/wp-content/uploads/2023/11/banner-001-painting-1920x1080-1.jpg',
-			title: 'test',
-			text: "Shaping Minds,<br> Nurturing Excellence,<br> <b style='text-shadow: 2px 2px 8px #000000' class='font-[800]'>Leading the Future</b>"
-		},
-		{
-			alt: 'test2',
-			src: 'https://rcis.in/wp-content/uploads/2023/11/banner-science-lab-1920x1080-003.jpg',
-			title: 'test2',
-			text: "Igniting Curiosity and<br> <b style='text-shadow: 2px 2px 8px #000000' class='font-[800]'>Fueling Future Innovators</b>"
-		},
-		{
-			alt: 'test2',
-			src: 'https://rcis.in/wp-content/uploads/2023/11/banner-001-fully-carpeted-terrace-for-sports-1920x1080-1.jpg',
-			title: 'test3',
-			text: "Unlocking Potential,<br> Fostering Creativity,<br> <b style='text-shadow: 2px 2px 8px #000000' class='font-[800]'>Holistic Development</b>"
-		},
-		{
-			alt: 'test2',
-			src: 'https://rcis.in/wp-content/uploads/2023/11/banner-003-robotics-1920x1080-2.jpg',
-			title: 'test4',
-			text: "Education Beyond Boundaries,<br> <b style='text-shadow: 2px 2px 8px #000000' class='font-[800]'>Empowering Future Achievers</b>"
-		},
-		{
-			alt: 'test2',
-			src: 'https://rcis.in/wp-content/uploads/2023/11/banner-m-001-amphitheater-1920x1080-1.jpg',
-			title: 'test5',
-			text: "Unleashing Potential,<br> Navigating Tomorrow's<br> <b style='text-shadow: 2px 2px 8px #000000' class='font-[800]'>Unknown Challenges</b>"
-		}
-	];
-
 	const sneakPeek = [
 		{
 			alt: 'test',
@@ -79,27 +46,21 @@
 	let index = 0;
 </script>
 
-<div class="bg-black">
-	<div class="heroSection h-screen relative bg-gradient-to-br from-red/70 to-purple/70">
+<div>
+	<div class="heroSection h-screen bg-cover">
 		<div class="absolute top-0 left-0 z-[100] w-full">
 			<Navbar />
 		</div>
-		<div class="carousel">
-			<Carousel {images} duration={3000} imgClass="rounded-[0]">
-				<div let:Slide slot="slide" let:index class="relative">
-					<Slide image={images[index]} />
-					{#key index}
-						<div class="absolute h-full items-end w-full flex justify-center">
-							<h1
-								transition:fly={{ y: 200, duration: 500 }}
-								class="text-5xl leading-tight font-[300] uppercase font-['Montserrat'] mb-24 text-white text-center"
-							>
-								{@html images[index].text}
-							</h1>
-						</div>
-					{/key}
-				</div>
-			</Carousel>
+		<div class="w-full h-1/2 items-center flex justify-end flex-col gap-4">
+			<h1
+				transition:fly={{ y: 200, duration: 500 }}
+				class="text-6xl leading-tight font-[300] uppercase font-['Montserrat'] text-white text-center"
+			>
+				Education Beyond Boundaries,<br />
+				<b style="text-shadow: 2px 2px 8px #000000" class="font-[800]"
+					>Empowering Future Achievers</b
+				>
+			</h1>
 		</div>
 	</div>
 	<div class="flex bg-[url('/SectionTwoBG.svg')] bg-cover bg-center innerShadow">
@@ -186,7 +147,7 @@
 				<Carousel images={sneakPeek} {forward} duration={3000} let:Controls bind:index>
 					<Controls />
 				</Carousel>
-				<br>
+				<br />
 				<Thumbnails images={sneakPeek} {forward} bind:index />
 			</div>
 		</div>
@@ -219,16 +180,16 @@
 		background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.6)),
 			url('https://rcis.in/wp-content/uploads/2023/09/Sports-Infrastructure-01.jpg');
 	}
-
-	:global(.carousel > div > div) {
-		height: 100vh;
-		border-radius: 0;
-	}
 	:global(.sneakPeek > div > div) {
 		height: 32rem;
 		border-radius: 0;
 	}
 	.innerShadow {
 		box-shadow: 0px 10px 18px rgba(0, 0, 0, 0.4) inset;
+	}
+	
+	.heroSection {
+		background-image: linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.6)),
+			url('https://rcis.in/wp-content/uploads/2023/10/Independence-Day-2023-24-new-006.jpg');
 	}
 </style>
