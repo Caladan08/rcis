76bb75c27790b80336eeed6e246b49506ffc1fae
Caladan8 <tnambiar2008@gmail.com>
2024-03-09 23:51:55 +0530

    Add semicolon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,5 +14,5 @@
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
 	@import url('https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200;300;400;500;600;700&display=swap');
-	@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap')
+	@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap');
 </style>
